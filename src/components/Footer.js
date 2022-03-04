0f9fcb32d615d0b7dcc91f57d8fe33aea321e790
ValdrinMurtezi <valdrinmurtezi1@gmail.com>
2022-03-04 14:53:50 +0100

    s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,26 @@ import "../styles/footer.scss";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer-container"></div>
+      <div className="footer-container">
+        <div className="contact">
+          <p>
+            <i className="fa-solid fa-phone"></i>+383 45 XXX XXX
+          </p>
+          <p>
+            <i className="fa-solid fa-envelope"></i>info@teknologies.com
+          </p>
+          <p>
+            <i className="fa-solid fa-globe"></i>www.teknologies.netlify.app
+          </p>
+        </div>
+        <div className="social-media">
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-github"></i>
+          <i className="fa-brands fa-youtube"></i>
+        </div>
+      </div>
     </div>
   );
 }
